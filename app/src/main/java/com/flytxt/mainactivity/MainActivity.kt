684c784bd1dca234a20a4b01ce4945fba0158d0c
Lord-Fifth <aditya.suresh098@gmail.com>
2019-07-07 10:35:35 +0530

    Data to Second Activity

--- app/src/main/java/com/flytxt/mainactivity/MainActivity.kt
+++ app/src/main/java/com/flytxt/mainactivity/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.flytxt.mainactivity
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +14,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Get text from EditText
+        val emailEt = findViewById<EditText>(R.id.email)
+        val otp = findViewById<EditText>(R.id.OAuth)
+        val subBtn = findViewById<Button>(R.id.submit)
+
         showRegistration()
 
-        submit.setOnClickListener {
+        subBtn.setOnClickListener {
+            email.isEnabled=false
+            val email = emailEt.text.toString()
+
             OAuth.visibility=View.VISIBLE
-            submit.setOnClickListener {
+
+            subBtn.setOnClickListener {
+                val auth = otp.text.toString()
+
+                //Intent to start activity
                 val intent = Intent(this,Success::class.java)
+
+                intent.putExtra("Email",email)
+                intent.putExtra("Token",auth)
+
                 startActivity(intent)
             }
         }
@@ -27,5 +44,14 @@ class MainActivity : AppCompatActivity() {
     private fun showRegistration(){
         OAuth.visibility=View.GONE
     }
+
+    /*
+    private fun isEmailValid(email:String):Boolean {
+        val expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"
+        val pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+    */
 
 }
--- app/src/main/java/com/flytxt/mainactivity/Success.kt
+++ app/src/main/java/com/flytxt/mainactivity/Success.kt
@@ -1,12 +1,24 @@
 package com.flytxt.mainactivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class Success : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_success)
+
+        //Get data from intent
+        var intent = intent
+        val name = intent.getStringExtra("Email")
+        val token = intent.getStringExtra("Token")
+
+        //TextView
+        val resultTv = findViewById<TextView>(R.id.resultTv)
+
+        //setText
+        resultTv.text = "Email : $name\nToken : $token"
     }
 }
